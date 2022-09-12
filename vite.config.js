47d1fa1e0773806a7740a23d7cde616b96d0ff93
苏常青 <2244915205>
2022-09-12 15:47:02 +0800

    成功创建 React 项目

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// 解析出  path 中的 resolve 参数
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      // 将 @ 指向 src 目录
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  },
+  server: {
+    // 本地开发服务器的端口
+    host: '0.0.0.0'
+  }
+})
